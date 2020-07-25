af53339c9c93bd1076721a4c7fd687d385e1d608
ridohyun <ri.dohyun@gmail.com>
2020-07-26 03:07:12 +0900

    Update react project

---
+++ React/likelion-react/my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//컴포넌트
+function App() {
+  return (
+    <div className="App">
+      <h1>hello!</h1>
+    </div>
+  );
+}
+
+//컴포넌트 수출
+export default App;
